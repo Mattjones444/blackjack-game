5f9c744830f6b2363a7e9ffcd98134a863dbd8c3
Matt Jones <mattjones@bjornbears-mbp.home>
2025-05-26 16:00:18 +0100

    Added functionality to add a new card

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 let firstCard=8;
 let secondCard=9;
+let cards=[firstCard,secondCard];
 let sum=firstCard+secondCard;
 let hasBlackJack = false;
 let isAlive=true;
 let message ="";
 let messageEl=document.getElementById("message-el");
-let sumEl=document.getElementById("sum-el");
+let sumEl=document.querySelector("#sum-el");
+let cardsEl=document.querySelector(".cards-el");
+
 
 function startGame(){
+    renderGame();
+}
+
+function renderGame(){
 if(sum<21){
     message="Do you want to draw a new card?";
 } else if (sum === 21){
@@ -17,10 +24,23 @@ if(sum<21){
     message="You're out of the game";
     isAlive=false;
 }
+
 messageEl.textContent=message;
 sumEl.textContent="Sum: "+sum;
+cardsEl.textContent="Cards: "+cards[0] + " " + cards[1];
+}
+
+function newCard(){
+    let card=1;
+    sum+=card;
+    renderGame();
 
 }
+
+
+
+
+
 
 
 
