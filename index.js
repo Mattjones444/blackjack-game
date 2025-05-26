982d4097a88396d2e9a50d6cabedc394545a87a9
Matt Jones <mattjones@bjornbears-mbp.home>
2025-05-26 13:42:38 +0100

    Added styling to the game and sum display

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-let firstCard=10;
-let secondCard=11;
+let firstCard=8;
+let secondCard=9;
 let sum=firstCard+secondCard;
 let hasBlackJack = false;
 let isAlive=true;
 let message ="";
+let messageEl=document.getElementById("message-el");
+let sumEl=document.getElementById("sum-el");
 
+function startGame(){
 if(sum<21){
     message="Do you want to draw a new card?";
 } else if (sum === 21){
@@ -14,8 +17,10 @@ if(sum<21){
     message="You're out of the game";
     isAlive=false;
 }
+messageEl.textContent=message;
+sumEl.textContent="Sum: "+sum;
 
-console.log(message);
+}
 
 
 
